67828b8f97ac5bf9f03c4e16d8a2f926541f13e8
Ethan Rietz <154637727+EthanRTZ@users.noreply.github.com>
2025-01-25 15:58:49 +0100

    Update client_article.py

--- controllers/client_article.py
+++ controllers/client_article.py
@@ -9,33 +9,47 @@ client_article = Blueprint('client_article', __name__,
                         template_folder='templates')
 
 @client_article.route('/client/index')
-@client_article.route('/client/article/show')              # remplace /client
-def client_article_show():                                 # remplace client_index
+@client_article.route('/client/article/show')
+def client_article_show():
     mycursor = get_db().cursor()
     id_client = session['id_user']
 
-    sql = '''   selection des articles   '''
-    list_param = []
-    condition_and = ""
-    # utilisation du filtre
-    sql3=''' prise en compte des commentaires et des notes dans le SQL    '''
-    articles =[]
+    sql = '''
+    SELECT s.*, t.libelle_type_ski as libelle
+    FROM ski s
+    LEFT JOIN type_ski t ON s.id_type_ski = t.id_type_ski
+    '''
+    mycursor.execute(sql)
+    articles = mycursor.fetchall()
 
+    # Pour le filtre
+    sql = '''SELECT * FROM type_ski'''
+    mycursor.execute(sql)
+    types_article = mycursor.fetchall()
 
-    # pour le filtre
-    types_article = []
+    # Calcul du panier
+    sql = '''
+    SELECT s.*, lp.quantite 
+    FROM ski s 
+    INNER JOIN ligne_panier lp ON s.id_ski = lp.id_ski 
+    WHERE lp.id_utilisateur = %s
+    '''
+    mycursor.execute(sql, (id_client,))
+    articles_panier = mycursor.fetchall()
 
-
-    articles_panier = []
-
+    prix_total = None
     if len(articles_panier) >= 1:
-        sql = ''' calcul du prix total du panier '''
-        prix_total = None
-    else:
-        prix_total = None
-    return render_template('client/boutique/panier_article.html'
-                           , articles=articles
-                           , articles_panier=articles_panier
-                           #, prix_total=prix_total
-                           , items_filtre=types_article
-                           )
+        sql = '''
+        SELECT SUM(s.prix_ski * lp.quantite) as prix_total
+        FROM ski s
+        INNER JOIN ligne_panier lp ON s.id_ski = lp.id_ski
+        WHERE lp.id_utilisateur = %s
+        '''
+        mycursor.execute(sql, (id_client,))
+        prix_total = mycursor.fetchone()['prix_total']
+
+    return render_template('client/boutique/panier_article.html',
+                         articles=articles,
+                         articles_panier=articles_panier,
+                         prix_total=prix_total,
+                         items_filtre=types_article)
