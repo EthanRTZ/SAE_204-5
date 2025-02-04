ae1b9986113e3d18c222c4b8e77b7070b86b5e84
Ethan Rietz <154637727+EthanRTZ@users.noreply.github.com>
2025-02-04 20:47:17 +0100

    Update connexion_db.py
    Signed-off-by: Ethan Rietz <154637727+EthanRTZ@users.noreply.github.com>

--- connexion_db.py
+++ connexion_db.py
@@ -1,23 +1,28 @@
-from flask import Flask, request, render_template, redirect, url_for, abort, flash, session, g
+#! /usr/bin/python
+# -*- coding:utf-8 -*-
+from flask import Flask, request, render_template, redirect, flash
 
+app = Flask(__name__)
+app.secret_key = 'une cle(token) : grain de sel(any random string)'
+
+from flask import session, g
 import pymysql.cursors
 
+import os                                 # à ajouter
+from dotenv import load_dotenv            # à ajouter
+load_dotenv()                             # à ajouter
+
 def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        #
-        db = g._database = pymysql.connect(
-            host="SAE2024grp20.mysql.pythonanywhere-services.com",
-            # host="localhost",
-            user="SAE2024grp20",
-            password="zqsd2025",
-            database="SAE2024grp20$default",
+    if 'db' not in g:
+        g.db =  pymysql.connect(
+            host=os.environ.get("HOST"),                # à modifier
+            user=os.environ.get("LOGIN"),               # à modifier
+            password=os.environ.get("PASSWORD"),        # à modifier
+            database=os.environ.get("DATABASE"),        # à modifier
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
-        # à activer sur les machines personnelles :
-        activate_db_options(db)
-    return db
+    return g.db
 
 def activate_db_options(db):
     cursor = db.cursor()
